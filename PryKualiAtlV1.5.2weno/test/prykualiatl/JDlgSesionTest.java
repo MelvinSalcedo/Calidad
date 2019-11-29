2bfe79f19815a1a59b88a7145f5f447315b67c99
Melvin <melvinsacledo169>
2019-11-29 14:41:47 -0500

    JDLgSesionTEst

---
+++ PryKualiAtlV1.5.2weno/test/prykualiatl/JDlgSesionTest.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prykualiatl;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author Orus
+ */
+public class JDlgSesionTest extends TestCase {
+    
+    public JDlgSesionTest(String testName) {
+        super(testName);
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /**
+     * Test of main method, of class JDlgSesion.
+     */
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        JDlgSesion.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    
+}
