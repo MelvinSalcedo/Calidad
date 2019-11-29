1544ef6b9deb89e46a68ba7fc38e81b5bbee3b3d
Melvin <melvinsacledo169>
2019-11-29 15:07:58 -0500

    TestBackuo

---
+++ PryKualiAtlV1.5.2weno/test/prykualiatl/JDlgBackupTest.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prykualiatl;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author Orus
+ */
+public class JDlgBackupTest extends TestCase {
+    
+    public JDlgBackupTest(String testName) {
+        super(testName);
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /**
+     * Test of seleccionar method, of class JDlgBackup.
+     */
+    public void testSeleccionar() {
+        System.out.println("seleccionar");
+        JDlgBackup instance = new JDlgBackup(null,true);
+        
+        System.out.println("nda");
+        instance.seleccionar();
+       
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of main method, of class JDlgBackup.
+     */
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        JDlgBackup.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+       // fail("The test case is a prototype.");
+    }
+    
+}
