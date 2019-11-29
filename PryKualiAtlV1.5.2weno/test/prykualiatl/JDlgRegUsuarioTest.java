2704be6972f6046ca59c15f1b078a9e201452d2e
katycyta <katicita1516@gmail.com>
2019-11-29 07:52:19 -0500

    Registro de usuarios - test passed

---
+++ PryKualiAtlV1.5.2weno/test/prykualiatl/JDlgRegUsuarioTest.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prykualiatl;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author katy
+ */
+public class JDlgRegUsuarioTest extends TestCase {
+    
+    public JDlgRegUsuarioTest(String testName) {
+        super(testName);
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /**
+     * Test of mostrar method, of class JDlgRegUsuario.
+     */
+    public void testMostrar() {
+        System.out.println("mostrar");
+        String cla = "";
+        String usu = "";
+        String contr = "";
+        boolean ara = false;
+        boolean b = false;
+        boolean c = false;
+        boolean d = false;
+        boolean e = false;
+        boolean f = false;
+        boolean g = false;
+        boolean h = false;
+        boolean i = false;
+        boolean j = false;
+        boolean k = false;
+        boolean l = false;
+        boolean m = false;
+        boolean n = false;
+        boolean o = false;
+        boolean p = false;
+        boolean q = false;
+        boolean r = false;
+        boolean s = false;
+        boolean t = false;
+        boolean u = false;
+        JDlgRegUsuario instance = new JDlgRegUsuario(null, true);
+        instance.mostrar(cla, usu, contr, ara, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u);
+        // TODO review the generated test code and remove the default call to fail.
+        if (instance==null){
+            fail("The test case is a prototype.");
+        }
+    }
+
+    /**
+     * Test of mostrar1 method, of class JDlgRegUsuario.
+     */
+    public void testMostrar1() {
+        System.out.println("mostrar1");
+        String guar = "";
+        //JDlgRegUsuario instance = null;
+        JDlgRegUsuario instance = new JDlgRegUsuario(null, true);
+        instance.mostrar1(guar);
+        // TODO review the generated test code and remove the default call to fail.
+        if (instance==null){
+            fail("The test case is a prototype.");
+        }
+    }
+
+    /**
+     * Test of ultimoregistro method, of class JDlgRegUsuario.
+     */
+    public void testUltimoregistro() {
+        System.out.println("ultimoregistro");
+        //JDlgRegUsuario instance = null;
+        JDlgRegUsuario instance = new JDlgRegUsuario(null, true);
+        instance.ultimoregistro();
+        // TODO review the generated test code and remove the default call to fail.
+        if (instance==null){
+            fail("The test case is a prototype.");
+        }
+    }
+
+    /**
+     * Test of main method, of class JDlgRegUsuario.
+     */
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        //JDlgRegUsuario.main(args);
+        JDlgRegUsuario instance = new JDlgRegUsuario(null, true);
+        // TODO review the generated test code and remove the default call to fail.
+        if (instance==null){
+            fail("The test case is a prototype.");
+        }
+    }
+    
+}
